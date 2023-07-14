3a3ba54e001d51fadb5236c7a763dd2747597b32
Carlos Franco <81891049+Carlosjfranco@users.noreply.github.com>
2023-07-14 18:01:35 -0500

    3er Cambio
    CARRUSEL

--- app.js
+++ app.js
@@ -8,6 +8,57 @@ if(document.readyState == 'loading'){
     ready();
 }
 
+// SLIDER DE LAS DONITAS
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+let slideInterval = setInterval(() => {
+  plusSlides(1);
+}, 3000);
+
+let slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+slideshowContainer.addEventListener('mouseenter', () => {
+  clearInterval(slideInterval);
+});
+
+slideshowContainer.addEventListener('mouseleave', () => {
+  slideInterval = setInterval(() => {
+    plusSlides(1);
+  }, 3000);
+});
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+
+
+
 function ready(){
     
     //Agregremos funcionalidad a los botones eliminar del carrito
@@ -231,10 +282,4 @@ function actualizarTotalCarrito(){
     document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
 
 }
-
-
-
-
-
-
 
